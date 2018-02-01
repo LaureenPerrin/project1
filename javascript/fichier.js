ed1ff8af9d38a79f9613ff6e424046d6c02ac888
Laureen Perrin <laureencoupe@hotmail.fr>
2018-02-01 15:46:25 +0100

    change tag (div slideshow) and its content in html and css. add js

--- javascript/fichier.js
+++ javascript/fichier.js
@@ -1,3 +1,39 @@
+$(function() {
+  $('a[href=#header]').click(function(){
+    $('html').animate({scrollTop:0}, 'slow');
+    return false;
+  });
+});
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
 function initMap() {
         var uluru = {lat: 48.881201, lng: 2.350838};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -10,9 +46,5 @@ function initMap() {
         });
       }
 
-$(function() {
-  $('a[href=#header]').click(function(){
-    $('html').animate({scrollTop:0}, 'slow');
-    return false;
-  });
-});
+
+
